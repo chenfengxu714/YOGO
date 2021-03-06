328bb1ec0fde2a7132cc5e3507db74b712e6f12e
Chenfeng Xu <43914865+chenfengxu714@users.noreply.github.com>
2021-03-07 01:28:19 +0800

    Update yogo.py

--- configs/s3dis/yogo/yogo.py
+++ configs/s3dis/yogo/yogo.py
@@ -9,10 +9,12 @@ configs.model.num_classes = configs.data.num_classes
 configs.model.token_l = 32
 configs.model.token_s = 128
 configs.model.token_c = 256
-configs.model.group_ = 'ball_query'
-configs.model.ball_r = 0.2
-
+configs.model.group = 'ball_query'
+configs.model.ball_r = 0.3
+configs.model.width_r = 1
+configs.model.norm = nn.InstanceNorm1d
 configs.model.extra_feature_channels = 6
+
 configs.dataset.num_points = 4096
 
 configs.train.optimizer.weight_decay = 0
